b11302023c502341ebb310f30edb4ee292a3a2c7
Gathua Kiragu <kiragugathua@gmail.com>
2017-06-01 08:26:07 +0300

    Added an onclick listener on the findLocation button

--- app/src/main/java/com/example/kiragu/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/kiragu/weatherapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.kiragu.weatherapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.findLocation)
+    Button mFindLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mFindLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Locations.class);
+                startActivity(intent);
+            }
+        });
     }
 }
