9ee2d36fe5a6e117be56ba0bbbb9e636ed08e96d
Gathua Kiragu <kiragugathua@gmail.com>
2017-06-01 07:59:43 +0300

    Added locations activity to display the responce from the Api

---
+++ app/src/main/java/com/example/kiragu/weatherapp/Locations.java
@@ -0,0 +1,13 @@
+package com.example.kiragu.weatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Locations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locations);
+    }
+}
